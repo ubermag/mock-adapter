c623630af9bd3d85b28fcd56c74f8782c800baa8
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-11 09:57:49 +0200

    Extend documentation of abstract classes to provide guidance to authors of adapters

--- ubermagcalculatorbase/base/evolver.py
+++ ubermagcalculatorbase/base/evolver.py
@@ -2,6 +2,16 @@ import micromagneticmodel as mm
 
 
 class Evolver(mm.abstract.Abstract):
-    """An abstract class for deriving evolvers."""
+    """An abstract class for deriving evolvers.
+
+    An evolver can be used to control numerical details of the simulation, e.g. the type
+    of gradient decent for energy minimisation or properties of the time integration.
+
+    Not all calculators provide extensive control over such parameters and implementing
+    evolvers in an adapter package is therefore optional. In simple cases it can be
+    easier to allow passing additional keyword arguments to the `Driver.drive` and
+    `Driver.schedule` method and to validate them in `Driver.drive_kwarg_setup` and
+    `Driver.schedule_kwarg_setup`.
+    """
 
     pass
