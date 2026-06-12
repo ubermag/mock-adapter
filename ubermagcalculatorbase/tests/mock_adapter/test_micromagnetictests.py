e5b9fdac500e6e09db3956c3bd87d8b8213cb0f0
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-12 14:13:37 +0200

    Placeholder for micromagnetictests

---
+++ ubermagcalculatorbase/tests/mock_adapter/test_micromagnetictests.py
@@ -0,0 +1,9 @@
+# Each adapater should import generic physics-based tests from `micromagnetictests`
+#
+# The tests in micromagnetictests will query capabilities of the adapter/calculator and
+# unsupported tests should be skipped automatically.
+# Some of the tests are regression tests, and failures of these could also indicate
+# wrong/inaccurate reference values
+
+# TODO activate tests
+# from micromagnetictests.calculatortests import *  # noqa: F403
