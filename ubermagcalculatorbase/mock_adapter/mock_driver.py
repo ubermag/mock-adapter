dd2af44d12ca880398f90a92d45e5cf2c43e118f
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-12 09:13:22 +0200

    Update mock driver

--- ubermagcalculatorbase/mock_adapter/mock_driver.py
+++ ubermagcalculatorbase/mock_adapter/mock_driver.py
@@ -1,25 +1,13 @@
 import json
 import pathlib
 import discretisedfield as df
-import micromagneticmodel as mm
-import abc
 
 from ubermagcalculatorbase.base import ExternalDriver
 from ubermagcalculatorbase import mock_adapter
+from ubermagcalculatorbase.mock_adapter._output_collecting_util import table_from_file
 
 
-class Driver(ExternalDriver):
-
-    @abc.abstractmethod
-    def _checkargs(self, kwargs):
-        pass
-
-    def schedule_kwargs_setup(self, schedule_kwargs):
-        self._checkargs(schedule_kwargs)
-
-    def drive_kwargs_setup(self, drive_kwargs):
-        self._checkargs(drive_kwargs)
-
+class _Driver(ExternalDriver):
     def _inputfilename(self, system):
         return f"{system.name}.input.json"
 
@@ -45,7 +33,12 @@ class Driver(ExternalDriver):
         # typically is no scheduling system and Python is always available.
         # Therefore, we return a Python comment that can be added to the
         # schedule script without breaking the execution.
-        return ["# run command line"]
+        if runner is None:
+            runner = mock_adapter.MockRunner()
+        return [
+            "# calculator-specific setup, e.g. setting environment variables",
+            "# " + runner._call(argstr=self._inputfilename(system), dry_run=True),
+        ]
 
     def _read_data(self, system):
         output_files = pathlib.Path(".").glob("m_*.hdf5")
@@ -56,34 +49,88 @@ class Driver(ExternalDriver):
         # - for better performance
         system.m.array = df.Field.from_file(str(last_outpup_file)).array
 
-        # TODO: update table information
-        # system.table = ut.Table.fromfile(f"{system.name}.odt", x=self._x)
+        # update table information
+        system.table = table_from_file(f"{system.name}.odt", x=self._x)
 
 
-class MinDriver(Driver):
+class MinDriver(_Driver):
+    """Energy minimisation with mock_calculator.
+
+    mock_calculator takes a single energy term Zeeman with uniform field direction and
+    rotates the initial magnetisation to that field direction in 10 steps.
+
+    Parameters
+    ----------
+
+    save_steps : bool, default: False
+
+        If set to True intermediate steps 1-9 of the energy minimisation are saved.
+        If False only the final step 10 is saved.
+    """
     _allowed_attributes = [
-        "convergence_mode",  # possible values: lin [default], log
         "save_steps",  # possible values: true, false [default]
     ]
 
-    def _checkargs(self, kwargs):
-        pass  # no kwargs should be checked
+    def schedule_kwargs_setup(self, schedule_kwargs):
+        """MinDriver takes no special keyword arguments."""
+        pass
 
+    def drive_kwargs_setup(self, drive_kwargs):
+        """MinDriver takes no special keyword arguments."""
+        pass
 
     def _check_system(self, system):
-        """Checks the system has energy in it"""
+        """Check that system.energy is defined."""
         if len(system.energy) == 0:
             raise RuntimeError("System's energy is not defined")
-        if not any(isinstance(term, mm.Zeeman) for term in system.energy):
-            raise RuntimeError("Zeeman energy must be defined")
 
     @property
     def _x(self):
         return "iteration"
 
 
-class TimeDriver(Driver):
+class TimeDriver(_Driver):
+    """Time integration with mock_calculator.
+
+    For time integration with mock_calculator damping is required and controls the
+    speed of relaxation. A precession term in the dynamics equation is ignored; instead
+    precession is hard-coded with period 1ns.
+    """
     _allowed_attributes = []
+
+    def schedule_kwargs_setup(self, schedule_kwargs):
+        """Additional keyword arguments for time drive.
+
+        Parameters
+        ----------
+        t : int, float
+
+            The end time of the time integration in seconds. Must be positive.
+
+        n : int
+
+            The number of steps to save during time integration. The first step is
+            saved at t/n, the last step at t. Must be positive.
+
+        """
+        self._checkargs(schedule_kwargs)
+
+    def drive_kwargs_setup(self, drive_kwargs):
+        """Additional keyword arguments for time drive.
+
+        Parameters
+        ----------
+        t : int, float
+
+            The end time of the time integration in seconds. Must be positive.
+
+        n : int
+
+            The number of steps to save during time integration. The first step is
+            saved at t/n, the last step at t. Must be positive.
+
+        """
+        self._checkargs(drive_kwargs)
 
     def _checkargs(self, kwargs):
         t, n = kwargs["t"], kwargs["n"]
@@ -98,13 +145,11 @@ class TimeDriver(Driver):
             raise ValueError(msg)
 
     def _check_system(self, system):
-        """Checks the system has dynamics in it"""
+        """Check that system.energy and system.dynamics are non-empty."""
         if len(system.dynamics) == 0:
             raise RuntimeError("System's dynamics is not defined")
         if len(system.energy) == 0:
             raise RuntimeError("System's energy is not defined")
-        if not any(isinstance(term, mm.Zeeman) for term in system.energy):
-            raise RuntimeError("Zeeman energy must be defined")
 
     @property
     def _x(self):
