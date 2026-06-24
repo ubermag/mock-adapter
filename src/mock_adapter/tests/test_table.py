3742fe9eda93f7a7859858497562dc30104c2ade
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-24 17:52:57 +0200

    Restructure fixtures for table tests

--- src/mock_adapter/tests/test_table.py
+++ src/mock_adapter/tests/test_table.py
@@ -6,59 +6,60 @@ import mock_adapter
 from mock_adapter.plugins import table_from_file
 
 
-def _table_energy_minimisation_factory(base_path):
-    def _inner(**kwargs):
-        # in this implementation we compute table data on the fly; for more expensive
-        # calculations pre-computed test samples can be committed
-        system = mm.examples.macrospin()
-        md = mock_adapter.MinDriver()
-        md.drive(system, dirname=base_path)
-
-        return table_from_file(
-            base_path / system.name / "drive-0" / "output.csv", **kwargs
-        )
-
-    return _inner
-
-
-def _table_llg_factory(base_path):
-    def _inner(**kwargs):
-        system = mm.examples.macrospin()
-        td = mock_adapter.TimeDriver()
-        td.drive(system, t=25e-12, n=10, dirname=base_path)
-
-        return table_from_file(
-            base_path / system.name / "drive-0" / "output.csv", **kwargs
-        )
-
-    return _inner
+@pytest.fixture(scope="session")
+def min_drive(tmp_path_factory):
+    dirname = tmp_path_factory.mktemp("min_drive")
+    # in this implementation we compute table data on the fly; for more expensive
+    # calculations pre-computed test samples can be committed
+    system = mm.examples.macrospin()
+    md = mock_adapter.MinDriver()
+    md.drive(system, dirname=dirname)
+    return dirname, system.name
 
 
 @pytest.fixture(scope="session")
-def table_llg_factory(tmp_path_factory):
+def time_drive(tmp_path_factory):
+    dirname = tmp_path_factory.mktemp("min_drive")
+    system = mm.examples.macrospin()
+    td = mock_adapter.TimeDriver()
+    td.drive(system, t=25e-12, n=10, dirname=dirname)
+    return dirname, system.name
+
+
+def _table_factory(dirname, name):
+    def _inner(**kwargs):
+        return table_from_file(dirname / name / "drive-0" / "output.csv", **kwargs)
+
+    return _inner
+
+
+@pytest.fixture
+def table_llg_factory(time_drive):
     """LLG tables."""
-    return _table_llg_factory(tmp_path_factory.mktemp("llg"))
+    dirname, name = time_drive
+    return _table_factory(dirname, name)
 
 
-@pytest.fixture(scope="session")
-def table_minimisation_factory(tmp_path_factory):
-    return _table_energy_minimisation_factory(tmp_path_factory.mktemp("min"))
+@pytest.fixture
+def table_minimisation_factory(min_drive):
+    dirname, name = min_drive
+    return _table_factory(dirname, name)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def table_hysteresis_factory():
     pytest.skip("Hysteresis not implemented.")
 
 
-@pytest.fixture(
-    scope="session", params=[_table_energy_minimisation_factory, _table_llg_factory]
-)
-def table_factory(request, tmp_path_factory):
+@pytest.fixture(params=["table_minimisation_factory", "table_llg_factory"])
+def table_factory(request):
     """Energy minimisation or LLG tables."""
-    return request.param(tmp_path_factory.mktemp("generic"))
+    return request.getfixturevalue(request.param)
 
 
 @pytest.fixture
-def table_llg_25ps(tmp_path):
+def table_llg_25ps(table_llg_factory):
     """LLG data with tmax=25ps."""
-    return _table_llg_factory(tmp_path)(x="t")
+    # In this implementation we only have a single LLG example with tmax = 25ps. Adapter
+    # packages can provide additional other samples.
+    return table_llg_factory(x="t")
